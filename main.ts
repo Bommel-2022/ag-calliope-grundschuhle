6afc32d8ad0be05e3f1ba56b92129717e4beacfc
Bommel-2022 <100511979+Bommel-2022@users.noreply.github.com>
2022-09-22 13:41:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,4 +6,28 @@ input.onButtonPressed(Button.A, function () {
         # . . . #
         # # # # #
         `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Wir sind Toni und Tom")
 })
